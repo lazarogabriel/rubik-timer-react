6ae148b24a66813cf64526dc768cd921335f3e59
lazarogabriel <lonofre@goiar.com>
2020-12-30 17:18:30 -0300

    Updated stats context to use reducer and actions

--- src/Contexts/StatsContext.js
+++ src/Contexts/StatsContext.js
@@ -1,11 +1,47 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
+import statsReducer from '../store/reducers/stats.reducer';
+import {
+    ADD_TIME_CARD,
+    DELETE_TIME_CARD, 
+    SAVE_TIME_CARD
+}from '../store/actions/stats.action';
 
 export const StatsContext = createContext();
 
 export const StatsProvider = (props) => {
-    const [timeCards, setTimeCard] = useState([]);
+    
+    const initialState = {
+        timeCards: []
+    };
+
+    const [state, dispatch] = useReducer(statsReducer, initialState);
+
+    const addTimeCard = (timeCard) => 
+    dispatch({
+        type: ADD_TIME_CARD, 
+        playload: timeCard 
+    });
+
+    const deleteTimeCard = (timeCardId) => 
+    dispatch({ 
+        type: DELETE_TIME_CARD,
+        playload: timeCardId 
+    });
+
+    const saveTimeCard = (timeCardId) => 
+    dispatch({ 
+        type: SAVE_TIME_CARD, 
+        playload: timeCardId 
+    });
+    
+
     return(
-        <StatsContext.Provider value={[timeCards, setTimeCard]}>
+        <StatsContext.Provider value={{
+            timeCards: state.timeCards,
+            addTimeCard,
+            deleteTimeCard,
+            saveTimeCard
+        }}>
             {props.children}
         </StatsContext.Provider>
     );
